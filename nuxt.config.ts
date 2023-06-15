26e00caea1c2f6df4ef650878c5a83bf0a6c4134
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-06-16 01:43:34 +1000

    Add sitemap render func and prerenderer config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,21 +7,16 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    "@nuxt/devtools"
+    "@nuxt/devtools",
   ],
   nitro: {
     output: {
       publicDir: path.join(__dirname, "built/public"),
     },
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
   },
   runtimeConfig: {},
   ssr: true,
-  // hooks: {
-  //   'vite:extendConfig'(config, {isClient}) {
-  //     if (process.env.NODE_ENV !== 'development' && isClient) {
-  //       config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
-  //       config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
-  //     }
-  //   }
-  // }
 });
