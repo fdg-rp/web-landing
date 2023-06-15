5ca3c7dacb881b64ca2796c79060ac80a0d4b461
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-06-15 22:36:30 +1000

    init 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,12 +16,12 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {},
   ssr: true,
-  hooks: {
-    'vite:extendConfig'(config, {isClient}) {
-      if (process.env.NODE_ENV !== 'development' && isClient) {
-        config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
-        config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
-      }
-    }
-  }
+  // hooks: {
+  //   'vite:extendConfig'(config, {isClient}) {
+  //     if (process.env.NODE_ENV !== 'development' && isClient) {
+  //       config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js'
+  //       config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js'
+  //     }
+  //   }
+  // }
 });
