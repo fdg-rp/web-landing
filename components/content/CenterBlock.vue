ed9f779b251a35ead97f45cca338be6043704e4f
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-06-15 22:50:05 +1000

    init 4

---
+++ components/content/CenterBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="fdg-center-block">
+    <ContentSlot :use="$slots.default" />
+  </section>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.fdg-center-block': {
+    textAlign: 'center',
+    paddingBottom: '128px'
+  },
+})
+</style>
