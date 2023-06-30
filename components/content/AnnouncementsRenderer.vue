e36b0944cecf234c13083d54361654a5ea5ed900
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-06-30 14:26:30 +1000

    fixes

--- components/content/AnnouncementsRenderer.vue
+++ components/content/AnnouncementsRenderer.vue
@@ -1,31 +1,33 @@
 <template>
-  <div v-for="(a, i) in data" :key="i">
-    <ProseH3
-      :id="
-        new Date(a.timestamp)
-          .toLocaleString('en-AU', {
-            dateStyle: 'medium',
-            timeStyle: 'long',
+  <ClientOnly>
+    <div v-for="(a, i) in data" :key="i">
+      <ProseH3
+        :id="
+          new Date(a.timestamp)
+            .toLocaleString('en-AU', {
+              dateStyle: 'medium',
+              timeStyle: 'long',
+            })
+            .replaceAll(' ', '-')
+        "
+      >
+        {{
+          new Date(a.timestamp).toLocaleString("en-AU", {
+            dateStyle: "medium",
+            timeStyle: "long",
           })
-          .replaceAll(' ', '-')
-      "
-    >
-      {{
-        new Date(a.timestamp).toLocaleString("en-AU", {
-          dateStyle: "medium",
-          timeStyle: "long",
-        })
-      }}
-    </ProseH3>
-    <ProseP>
-      <Icon name="noto:loudspeaker"></Icon> By
-      <ProseEm> {{ a.authorName }} </ProseEm> in
-      <ProseStrong> Community Announcements </ProseStrong>
-    </ProseP>
-    <ProseP>
-      {{ a.content }}
-    </ProseP>
-  </div>
+        }}
+      </ProseH3>
+      <ProseP>
+        <Icon name="noto:loudspeaker"></Icon> By
+        <ProseEm> {{ a.authorName }} </ProseEm> in
+        <ProseStrong> Community Announcements </ProseStrong>
+      </ProseP>
+      <ProseP>
+        {{ a.content }}
+      </ProseP>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup>
