3857053564fbb3979b752edecb1c2561510469d6
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-07-10 17:17:15 +1000

    fix url

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find();
   const sitemap = new SitemapStream({
-    hostname: "https://docs.fatduckgaming.com",
+    hostname: "https://preview.fatduckgaming.com",
   });
   for (const doc of docs) {
     sitemap.write({
