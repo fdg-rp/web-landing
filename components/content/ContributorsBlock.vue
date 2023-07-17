08a26f7d2bfbe6c111b087f4144d03237a0c70f3
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-07-17 17:30:37 +1000

    added contributors to site

---
+++ components/content/ContributorsBlock.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ProseH1 id="contributors">Without you, FDG wouldn't be here.</ProseH1>
+    <ProseP
+      style="
+        color: var(--elements-text-secondary-color-static) !important;
+        font-size: var(--text-lg-fontSize) !important;
+      "
+    >
+      <ContentSlot :use="$slots.default" />
+    </ProseP>
+    <ClientOnly fallback-tag="span" fallback="Loading contributors..." v-once>
+      <div class="contributors">
+        <div v-for="i in data">
+          <a :href="i.html_url" target="_blank" rel="noopener noreferrer">
+            <img :src="i.avatar_url" />
+          </a>
+        </div>
+      </div>
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("fdg-announcements", () =>
+  $fetch("https://api.github.com/repos/fdg-rp/web-landing/contributors")
+);
+</script>
+
+<style>
+.contributors {
+  margin-top: 56px;
+  display: grid;
+  grid-template-columns: repeat(10, minmax(0, 1fr));
+  gap: 1rem;
+}
+
+.contributors img {
+  border-radius: 50%;
+}
+</style>
