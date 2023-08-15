d6ac728d7a10cf95b3bc8bfed549fddd2cefa283
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2023-08-15 18:28:03 +1000

    Update AnnouncementsRenderer.vue

--- components/content/AnnouncementsRenderer.vue
+++ components/content/AnnouncementsRenderer.vue
@@ -23,9 +23,6 @@
         <ProseEm> {{ a.authorName }} </ProseEm> in
         <ProseStrong> Community Announcements </ProseStrong>
       </ProseP>
-      <ProseP v-if="a.image">
-        <img :src="a.image" alt="Discord Image" />
-      </ProseP>
       <ProseP>
         {{ a.content }}
       </ProseP>
