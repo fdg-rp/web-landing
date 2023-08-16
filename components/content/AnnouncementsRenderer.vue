1d230eb748c7e9dde2c156131fbc3d1bc2b1271f
Macca <139609899+maccag69@users.noreply.github.com>
2023-08-16 15:17:16 +1000

    Update AnnouncementsRenderer.vue
    - makes it look prettier
    - tried to make the most of all the api available
    - added discord pfp to the next of name

--- components/content/AnnouncementsRenderer.vue
+++ components/content/AnnouncementsRenderer.vue
@@ -19,12 +19,15 @@
         }}
       </ProseH3>
       <ProseP>
-        <Icon name="noto:loudspeaker"></Icon> By
-        <ProseEm> {{ a.authorName }} </ProseEm> in
+        <Icon name="ic:round-announcement"></Icon> By
+        <ProseEm class="author">
+          <img :src="a.authorImg" alt="PFP">
+          {{ a.authorName }} 
+        </ProseEm> in
         <ProseStrong> Community Announcements </ProseStrong>
       </ProseP>
-      <ProseP>
-        {{ a.content }}
+      <ProseP class="announcement">
+        <a :href="a.url"> {{ a.content }} </a>
       </ProseP>
     </div>
   </ClientOnly>
@@ -35,3 +38,43 @@ const { data } = await useAsyncData("fdg-announcements", () =>
   $fetch("https://api.fatduckgaming.com/announcements")
 );
 </script>
+
+<style>
+.announcement {
+  position: relative;
+  background-color: #7289da33;
+  border-radius: 10px;
+  border: solid 2px;
+  border-color: #7289da;
+  padding: 15px;
+  width: fit-content;
+  height: fit-content;
+  box-shadow: inset 0 0 0 0 #7289da;
+  transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+} 
+
+.announcement:hover {
+  box-shadow: inset 1000px 0 0 0 #7289da;
+  cursor: pointer;
+}
+
+.author {
+  background-color: #7289da;
+  border-radius: 10px;
+  border: solid 2px;
+  border-color: #5d78d8;
+  padding: 10px;
+  width: fit-content;
+  height: fit-content;
+  color: #1e2124;
+  display: flex;
+  align-items: center;
+}
+
+.author img {
+  margin-right: 1rem;
+  border-radius: 50px;
+  height: 40px;
+  width: 40px;
+}
+</style>
